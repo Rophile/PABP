92e2ac5347308a9b97790e5edb1d34dd58c95d84
Rophile <syailafaagna07@gmail.com>
2026-05-09 14:21:33 +0700

    automatically assign a namespace to any older plugin

--- snapbooth_mobile/android/build.gradle.kts
+++ snapbooth_mobile/android/build.gradle.kts
@@ -19,6 +19,22 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    afterEvaluate {
+        if (project.extensions.findByName("android") != null) {
+            val android = project.extensions.getByName("android")
+            val getNamespace = android.javaClass.methods.find { it.name == "getNamespace" }
+            val setNamespace = android.javaClass.methods.find { it.name == "setNamespace" && it.parameterTypes.size == 1 && it.parameterTypes[0] == String::class.java }
+            
+            if (getNamespace != null && setNamespace != null) {
+                if (getNamespace.invoke(android) == null) {
+                    setNamespace.invoke(android, project.group.toString())
+                }
+            }
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
