41942aaf21cb5f56b99e41b033b448410b6c79b4
Rophile <syailafaagna07@gmail.com>
2026-05-09 14:25:23 +0700

    Update the "Hack" for Kotlin DSL

--- snapbooth_mobile/android/build.gradle.kts
+++ snapbooth_mobile/android/build.gradle.kts
@@ -15,26 +15,15 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
-subprojects {
-    project.evaluationDependsOn(":app")
+tasks.register<Delete>("clean") {
+    delete(rootProject.layout.buildDirectory)
 }
 
 subprojects {
     afterEvaluate {
         if (project.extensions.findByName("android") != null) {
-            val android = project.extensions.getByName("android")
-            val getNamespace = android.javaClass.methods.find { it.name == "getNamespace" }
-            val setNamespace = android.javaClass.methods.find { it.name == "setNamespace" && it.parameterTypes.size == 1 && it.parameterTypes[0] == String::class.java }
-            
-            if (getNamespace != null && setNamespace != null) {
-                if (getNamespace.invoke(android) == null) {
-                    setNamespace.invoke(android, project.group.toString())
-                }
-            }
+            val android = project.extensions.getByName("android") as com.android.build.gradle.BaseExtension
+            android.namespace = project.group.toString()
         }
     }
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
 }
